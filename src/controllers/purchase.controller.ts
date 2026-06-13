d68741e82f9450edc1ee0eaeb009d10cddd837bf
andreiveisuade <aveis@uade.edu.ar>
2026-06-13 19:34:48 -0300

    refactor(purchase): ejemplo de DIP con PurchaseRepository inyectado en PurchaseService

--- src/controllers/purchase.controller.ts
+++ src/controllers/purchase.controller.ts
@@ -1,13 +1,14 @@
 import { asyncHandler } from '../utils/asyncHandler';
-import * as purchaseService from '../services/purchase.service';
+import { purchaseService } from '../services/purchase.service';
+import type { AuthedRequest } from '../types/http';
 
-export const list = asyncHandler(async (req, res) => {
+export const list = asyncHandler<AuthedRequest>(async (req, res) => {
   const status = typeof req.query.status === 'string' ? req.query.status : undefined;
-  const purchases = await purchaseService.list(req.user!.id, status);
+  const purchases = await purchaseService.list(req.user.id, status);
   res.json(purchases);
 });
 
-export const getById = asyncHandler(async (req, res) => {
-  const purchase = await purchaseService.getById(req.user!.id, String(req.params.id));
+export const getById = asyncHandler<AuthedRequest>(async (req, res) => {
+  const purchase = await purchaseService.getById(req.user.id, String(req.params.id));
   res.json(purchase);
 });
